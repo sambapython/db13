123a005099fbf82287a43f8f9306e8ec44ec123d
sambapython <sambapython@gmail.com>
2019-09-22 09:01:16 +0530

    2019-09-22 popuate the data into db using models

--- NETINFO/info/models.py
+++ NETINFO/info/models.py
@@ -7,7 +7,7 @@ create table gateway(name varchar(250))
 # Create your models here.
 class Gateway(models.Model):
 	name = models.CharField(max_length=300) # blank=True, null=True, unique=True, primary_key=True
-	ip = models.GenericIPAddressField()
+	ip = models.GenericIPAddressField(unique=True)
 	mask = models.GenericIPAddressField()
 	route = models.GenericIPAddressField()
 	interface_name=models.CharField(max_length=300)
